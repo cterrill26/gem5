0465ef4d5a5d94b87e04b796e5a1e24c3c6c607f
Caleb Terrill <cdterrill26@gmail.com>
2021-03-17 23:52:29 +0000

    cpu: change to single load
    Change-Id: I467fb23334be7f03001421682df247366d746af9

--- src/matrix/matrix_unit.hh
+++ src/matrix/matrix_unit.hh
@@ -1,13 +1,23 @@
-#include <cfloat>
-
 #ifndef __MATRIX_UNIT_HH__
 #define __MATRIX_UNIT_HH__
 
+#include <cstdio>
+
+class Mat{
+public:
+  int vals[16];
+};
+
+class Vec{
+public:
+  int vals[16];
+};
+
 class MatrixUnit{
 public:
-  int a[16];
-  int b[16];
-  int c[16];
+  Mat a;
+  Mat b;
+  Mat c;
   //int scalar;
   //int vector[4];
 
@@ -16,9 +26,9 @@ public:
       for (int j = 0; j < 4; ++j) {
         int sum = 0;
         for (int k = 0; k < 4; ++k) {
-          sum += a[i * 4 + k] * b[k * 4 + j];
+          sum += a.vals[i * 4 + k] * b.vals[k * 4 + j];
         }
-        c[i * 4 + j] += sum;
+        c.vals[i * 4 + j] += sum;
       }
     }
   }
@@ -28,7 +38,7 @@ public:
       int row = i * 4;
       for (int j = 0; j < 4; ++j) {
         int index = row + j;
-        c[index] = a[index] + b[index];
+        c.vals[index] = a.vals[index] + b.vals[index];
       }
     }
   }
@@ -36,8 +46,8 @@ public:
   void transposea(){
         for (int i = 0; i < 4; ++i) {
           for (int j = 0; j < 4; ++j) {
-            c[i * 4 + j] = a[j * 4 + i];
-            c[j * 4 + i] = a[i * 4 + j];
+            c.vals[i * 4 + j] = a.vals[j * 4 + i];
+            c.vals[j * 4 + i] = a.vals[i * 4 + j];
           }
         }
   }
@@ -46,8 +56,8 @@ public:
   void transposeb(){
         for (int i = 0; i < 4; ++i) {
           for (int j = 0; j < 4; ++j) {
-            c[i * 4 + j] = b[j * 4 + i];
-            c[j * 4 + i] = b[i * 4 + j];
+            c.vals[i * 4 + j] = b.vals[j * 4 + i];
+            c.vals[j * 4 + i] = b.vals[i * 4 + j];
           }
         }
   }
@@ -58,16 +68,16 @@ public:
         int sum = 0;
         int row = i * 4;
         for (int j = 0; j < 4; ++j) {
-           sum += a[row + j] * b[j];
+           sum += a.vals[row + j] * b.vals[j];
         }
-        c[i] += sum;
+        c.vals[i] += sum;
     }
   }
 
   void matrix_scalar_multiplya(int param) {
       for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
-          c[i * 4 + j] = a[i * 4 + j] * param;
+          c.vals[i * 4 + j] = a.vals[i * 4 + j] * param;
         }
       }
   }
@@ -75,7 +85,7 @@ public:
   void matrix_scalar_multiplyb(int param) {
       for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
-          c[i * 4 + j] = b[i * 4 + j] * param;
+          c.vals[i * 4 + j] = b.vals[i * 4 + j] * param;
         }
       }
   }
